45d2740aecb16ef86799c5f25c81689b333c5bfd
Philipp Andert <philipp.andert@iteratec.com>
2024-07-18 21:50:11 +0200

    memoize kanjivg calls

--- src/services/KanjiService.ts
+++ src/services/KanjiService.ts
@@ -1,19 +1,20 @@
 import { KanaKanji } from "../model/KanaKanji";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import memoize from "memoizee";
 
 const url: string = "https://kanjivg.tagaini.net/kanjivg/kanji/";
 
-export const isKanji = (hexCode: number): boolean => {
+const isKanji = (hexCode: number): boolean => {
     return 0x4e00 < hexCode && hexCode <= 0x9fff;
 };
 
-export const isKana = (hexCode: number): boolean => {
+const isKana = (hexCode: number): boolean => {
     const isHiragana = 0x3040 < hexCode && hexCode <= 0x309f;
     const isKatakana = 0x30A0 < hexCode && hexCode <= 0x30ff;
     return isHiragana || isKatakana;
 };
 
-export const getUnicodes = (vocab: string): string[] => {
+const getUnicodes = (vocab: string): string[] => {
     const codes: string[] = [];
     for (let i = 0; i < vocab.length; i++) {
         const decimalCodePoint = vocab.codePointAt(i);
@@ -23,7 +24,7 @@ export const getUnicodes = (vocab: string): string[] => {
     return codes;
 };
 
-export const getKanjiIndices = (unicodes: string[]): number[] => {
+const getKanjiIndices = (unicodes: string[]): number[] => {
     return unicodes
         .map((code, index) => {
             const hexCode = parseInt(code, 16);
@@ -32,7 +33,7 @@ export const getKanjiIndices = (unicodes: string[]): number[] => {
         .filter(index => index !== -1);
 };
 
-export const getKanaIndices = (unicodes: string[]): number[] => {
+const getKanaIndices = (unicodes: string[]): number[] => {
     return unicodes
         .map((code, index) => {
             const hexCode = parseInt(code, 16);
@@ -41,14 +42,18 @@ export const getKanaIndices = (unicodes: string[]): number[] => {
         .filter(index => index !== -1);
 };
 
-export const getKanaKanji = async (unicodes: string[], kanjiIndices: number[], kanaIndices: number[]) => {
+const memoizedFetchKanji = memoize((url: string, unicode: string): Promise<AxiosResponse> => {
+    return axios.get(`${url}/${unicode}.svg`);
+});
+
+const getKanaKanji = async (unicodes: string[], kanjiIndices: number[], kanaIndices: number[]) => {
     const kanaKanji: KanaKanji[] = [];
     for (let i = 0; i < unicodes.length; i++) {
         const isKanji = kanjiIndices.includes(i);
         const isKana = kanaIndices.includes(i);
         if (!isKanji && !isKana) { continue; }
 
-        const svgPath = (await axios.get(`${url}/${unicodes[i]}.svg`) as any).data;
+        const svgPath = (await memoizedFetchKanji(url, unicodes[i]) as any).data;
         kanaKanji.push(<KanaKanji>{
             hexCode: unicodes[i],
             isKanji,
@@ -57,4 +62,13 @@ export const getKanaKanji = async (unicodes: string[], kanjiIndices: number[], k
         });
     }
     return kanaKanji;
+};
+
+export {
+    isKanji,
+    isKana,
+    getUnicodes,
+    getKanjiIndices,
+    getKanaIndices,
+    getKanaKanji
 };
